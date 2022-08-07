e40d87b8bb4de08a1eed1b589e5c56e8070e57ee
Utsav <utsavpathak59@gmail.com>
2022-08-07 17:25:30 +0530

    restructuring and flash setup

--- yelpcamp/models/campground.js
+++ yelpcamp/models/campground.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 const CampGroundSchema = new Schema({
     title: String,
     price: Number,
     image: String,
     description : String,
-    location: String
+    location: String,
+    reviews:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
+
+CampGroundSchema.post('findOneAndDelete', async function(doc){
+    if(doc){
+        await Review.remove({
+            _id:{
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground' , CampGroundSchema)
