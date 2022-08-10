fb59cb2b9767b79b3c0033d95867a3bd8f3422cd
Utsav <utsavpathak59@gmail.com>
2022-08-10 15:56:20 +0530

    added in maps using MapBox

--- yelpcamp/routes/campgrounds.js
+++ yelpcamp/routes/campgrounds.js
@@ -10,7 +10,7 @@ const upload = multer({ storage })
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn,upload.array('image'), validateCampground ,catchAsync (campgrounds.createCampground));
+    .post(isLoggedIn,upload.array('image'),catchAsync (campgrounds.createCampground));
 
 
 router.get('/new', isLoggedIn,campgrounds.renderNewForm);
